85bf9ba6c09d999ad7dc171a635ba9262ed21a04
Gunachandru <tgunachandru@gmail.com>
2026-06-10 18:24:29 +0530

    copleted SingletonPatternExample

--- week1/SingletonPatternExample/src/Main.java
+++ week1/SingletonPatternExample/src/Main.java
@@ -1,15 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Logger {
+    private static Logger loginInstance = null;
+   public String str;
+    private Logger() {
+        str="Logger initialised";
+    }
+    public static Logger Logger(){
+        if(loginInstance==null){
+            loginInstance=new Logger();
+        }
+        return loginInstance;
+}}
+
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+       Logger one=Logger.Logger();
+       Logger two=Logger.Logger();
+       System.out.println("Message of one "+one.str+" "+one.hashCode());
+       System.out.println("Message of two "+two.str+" "+two.hashCode());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
+
 }
