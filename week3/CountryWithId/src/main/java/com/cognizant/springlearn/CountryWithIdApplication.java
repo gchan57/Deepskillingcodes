199b1869459b4b54889864f3a67e675174d21283
gchan57 <tgunachandru@gmail.com>
2026-07-14 21:26:30 +0530

    added country with id

---
+++ week3/CountryWithId/src/main/java/com/cognizant/springlearn/CountryWithIdApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.springlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryWithIdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryWithIdApplication.class, args);
+	}
+
+}
